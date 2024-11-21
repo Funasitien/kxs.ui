5644177db0410c89ec150a8df91a8d912a925aa6
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-11-21 19:28:07 +0100

    ✨ Layout Projet

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,11 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    extendMarkdownConfig: (config) => {
+      config.linkify = true;
+      return config;
+    }
+  },
   integrations: [tailwind(), icon(), mdx()]
 });
