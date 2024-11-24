46a33ebba8207acff9ba5c390780d2616a129abb
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-11-24 10:37:04 +0100

    🔥 Clean & Bugfixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ export default {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["light", "dark", "sunset"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
